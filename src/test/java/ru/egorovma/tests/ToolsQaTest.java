600ec5844b2ca1657670640f53160f3317afe110
egorov-ma <link@egorovma.ru>
2024-01-05 20:31:54 +0400

    upd registration test

--- src/test/java/ru/egorovma/tests/ToolsQaTest.java
+++ src/test/java/ru/egorovma/tests/ToolsQaTest.java
@@ -1,5 +1,8 @@
 package ru.egorovma.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
 import ru.egorovma.data.TestDataForStudentRegistrationForm;
 import ru.egorovma.pages.RegistrationPage;
 import org.junit.jupiter.api.DisplayName;
@@ -15,13 +18,12 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static io.qameta.allure.Allure.step;
 import static ru.egorovma.data.TestDataForStudentRegistrationForm.CSS_EXPECTED_VALUE;
 import static ru.egorovma.data.TestDataForStudentRegistrationForm.TOOLS_QA_URL;
 
-
-//todo попробовать запускать через gradle только SMOKE тесты
-
 @DisplayName("Проверка экранной формы регистрации студента")
+@Tag("Registration")
 public class ToolsQaTest extends TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
     TestDataForStudentRegistrationForm data = new TestDataForStudentRegistrationForm("en");
@@ -61,33 +63,45 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Positive")
     })
+    @Feature("Заполнение формы регистрации студента")
+    @Story("Позитивный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Проверка заполнения всех полей")
     void studentFullRegistrationForm(TestDataForStudentRegistrationForm data) {
-        registrationPage.openPage(TOOLS_QA_URL)
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.emailAddress)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth)
-                .setSubject(data.subjects)
-                .setHobbies(data.hobbies)
-                .setPicture(data.picture)
-                .setCurrentAddress(data.address)
-                .setState(data.state)
-                .setCity(data.city)
-                .submit();
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Заполняем все поля формы", () -> {
+            registrationPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.emailAddress)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth)
+                    .setSubject(data.subjects)
+                    .setHobbies(data.hobbies)
+                    .setPicture(data.picture)
+                    .setCurrentAddress(data.address)
+                    .setState(data.state)
+                    .setCity(data.city);
+        });
+        step("Отправляем форму регистрации", () -> registrationPage.submit());
 
-        registrationPage.checkResult("Student Name", data.fullName)
-                .checkResult("Student Email", data.emailAddress)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber)
-                .checkResult("Date of Birth", data.dateOfBirth)
-                .checkResult("Subjects", data.subjects)
-                .checkResult("Hobbies", data.hobbies)
-                .checkResult("Picture", data.picture)
-                .checkResult("Address", data.address)
-                .checkResult("State and City", data.stateAndCity);
+        step("Проверка результата регистрации", () -> {
+            registrationPage.checkResult("Student Name", data.fullName)
+                    .checkResult("Student Email", data.emailAddress)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber)
+                    .checkResult("Date of Birth", data.dateOfBirth)
+                    .checkResult("Subjects", data.subjects)
+                    .checkResult("Hobbies", data.hobbies)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("Address", data.address)
+                    .checkResult("State and City", data.stateAndCity);
+        });
     }
 
     @MethodSource("dataProvider")
@@ -96,20 +110,32 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Positive")
     })
+    @Feature("Заполнение формы регистрации студента")
+    @Story("Позитивный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Проверка заполнения минимального кол-ва полей")
     void studentMinimumRegistrationForm(TestDataForStudentRegistrationForm data) {
-        registrationPage.openPage(TOOLS_QA_URL)
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth)
-                .submit();
-
-        registrationPage.checkResult("Student Name", data.fullName)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber)
-                .checkResult("Date of Birth", data.dateOfBirth);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Заполняем обязательные поля", () -> {
+            registrationPage
+                    .setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth);
+        });
+        step("Отправляем форму регистрации", () -> registrationPage.submit());
+        step("Проверка результата регистрации с минимальным количеством полей", () -> {
+            registrationPage.checkResult("Student Name", data.fullName)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber)
+                    .checkResult("Date of Birth", data.dateOfBirth);
+        });
     }
 
     @CsvFileSource(resources = "/test_data/studentTestData.csv")
@@ -118,21 +144,32 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Positive")
     })
-    @DisplayName("Проверяем работу с календарем")
+    @Feature("Проверяем работу календаря")
+    @Story("Позитивный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Проверяем различные наборы дат")
     void calendarTest(String firstName, String lastName, String gender, String userNumber,
                       String dayBirth, String monthBirth, String yearBirth) {
-        registrationPage.openPage(TOOLS_QA_URL)
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setGender(gender)
-                .setUserNumber(userNumber)
-                .setDateOfBirth(dayBirth, monthBirth, yearBirth)
-                .submit();
-
-        registrationPage.checkResult("Student Name", firstName + " " + lastName)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", userNumber)
-                .checkResult("Date of Birth", dayBirth + " " + monthBirth + "," + yearBirth);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Заполняем обязательные поля", () -> {
+            registrationPage.setFirstName(firstName)
+                    .setLastName(lastName)
+                    .setGender(gender)
+                    .setUserNumber(userNumber)
+                    .setDateOfBirth(dayBirth, monthBirth, yearBirth);
+        });
+        step("Отправляем форму регистрации", () -> registrationPage.submit());
+        step("Проверка результата выбора дат в календаре", () -> {
+            registrationPage.checkResult("Student Name", firstName + " " + lastName)
+                    .checkResult("Gender", gender)
+                    .checkResult("Mobile", userNumber)
+                    .checkResult("Date of Birth", dayBirth + " " + monthBirth + "," + yearBirth);
+        });
     }
 
     @MethodSource("studentHobbiesTest")
@@ -141,22 +178,32 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Positive")
     })
+    @Feature("Проверка хобби")
+    @Story("Позитивный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.MINOR)
+    @Link(url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Проверяем разные наборы хобби")
     void studentHobbiesTest(List<String> hobbies) {
-        registrationPage.openPage(TOOLS_QA_URL)
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth)
-                .setHobbies(hobbies)
-                .submit();
-
-        registrationPage.checkResult("Student Name", data.fullName)
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Заполняем обязательные поля", () -> {
+            registrationPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.dayBirth, data.monthBirth, data.yearBirth)
+                    .setHobbies(hobbies);
+        });
+        step("Отправляем форму регистрации", () -> registrationPage.submit());
+        step("Проверка результата выбора хобби", () ->
+                registrationPage.checkResult("Student Name", data.fullName)
                 .checkResult("Gender", data.gender)
                 .checkResult("Mobile", data.userNumber)
                 .checkResult("Date of Birth", data.dateOfBirth)
-                .checkResult("Hobbies", hobbies);
+                .checkResult("Hobbies", hobbies));
     }
 
     @Test
@@ -164,10 +211,20 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Negative")
     })
+    @Feature("Проверка обязательных полей")
+    @Story("Негативный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.NORMAL)
+    @Link(url = "https://demoqa.com/automation-practice-form")
     @DisplayName("Проверка отправки не заполненной ЭФ")
     void negativeStudentRegistrationForm() {
-        registrationPage.openPage(TOOLS_QA_URL).submit();
-        registrationPage.checkRequiredFields("border-color", CSS_EXPECTED_VALUE);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Отправляем не заполненную форму регистрации", () -> registrationPage.submit());
+        step("Проверка подсвечивания обязательных полей", () ->
+                registrationPage.checkRequiredFields("border-color", CSS_EXPECTED_VALUE));
     }
 
     @ValueSource(strings = {
@@ -178,11 +235,22 @@ public class ToolsQaTest extends TestBase {
             @Tag("WEB"),
             @Tag("Negative")
     })
-    @DisplayName("Проверка мобильного телефона")
+    @Feature("Проверка мобильного телефона")
+    @Story("Негативный тест")
+    @Owner("@egorovma")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link(url = "https://demoqa.com/automation-practice-form")
+    @DisplayName("Проверка различных наборов мобильного телефона")
     void userNumberTest(String userNumber) {
-        registrationPage.openPage(TOOLS_QA_URL).submit();
-        registrationPage.setUserNumber(userNumber)
-                .submit();
-        registrationPage.checkRequiredUserNumber("border-color", CSS_EXPECTED_VALUE);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу", () -> {
+            registrationPage.openPage(TOOLS_QA_URL);
+        });
+        step("Заполнение номера телефона", () -> {
+            registrationPage.setUserNumber(userNumber);
+        });
+        step("Отправляем форму регистрации", () -> registrationPage.submit());
+        step("Проверяем наличие ошибки в заполнении номара телефона", () ->
+                registrationPage.checkRequiredUserNumber("border-color", CSS_EXPECTED_VALUE));
     }
 }
