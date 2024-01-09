4ba948d8f2b6cd0dd8e591da4faf5fa4df5546bc
Maksim Egorov <m.egorov193@gmail.com>
2024-01-09 18:25:05 +0400

    added video

--- src/test/java/ru/egorovma/tests/TestBase.java
+++ src/test/java/ru/egorovma/tests/TestBase.java
@@ -5,7 +5,10 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.egorovma.helpers.Attach;
+
+import java.util.Map;
 
 public class TestBase {
 
@@ -14,8 +17,16 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
+
         SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
